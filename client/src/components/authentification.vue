1b38b0ec4390c4e031d505d6faf4908f838aa005
Cheik <cheik.kone@hetic.net>
2019-06-19 23:32:35 +0200

    auto

--- client/src/components/authentification.vue
+++ client/src/components/authentification.vue
@@ -1,36 +1,58 @@
 <template>
-       <form>
-                <h1>Connexion</h1>
-                
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-                <input type="submit" id='submit' value='LOGIN' >
-            </form>
+<div class="ok" v-on:click="checkForm"></div>
 </template>
+
+<style lang="scss" scoped>
+.ok{
+  height: 100px;
+  width: 100px;
+  background-color: black;
+}
+form{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 250px;
+}
+</style>
 
 <script>
 import factService from '../factService' 
 
+
 export default {
-  name: 'HelloWorld',
+  name: 'authentification',
   data() {
     return {
+
     }
   },
+
   async created() {
     try {
       this.facts = await factService.getFacts();
     } catch (err) {
       this.error = err.message;
     }  
+  },
+
+    methods: {
+    checkForm(e) {
+      fetch( 'http://localhost:5000/api/facts/authentification/okkkkkk' ,{
+      method: 'POST',
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+      body : JSON.stringify({ 
+        mail :"contact@mail.com" 
+       ,password :"Kone"
+        })
+    }
+      );
+      e.preventDefault();
+    },
+    
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './components/Home';
 import Document from './components/Document';
+import authentification from './components/authentification.vue';
 
 Vue.use(VueRouter);
 
@@ -16,5 +17,9 @@ export default new VueRouter({
      path : '/document',
      component: Document,
    },
+   {
+    path : '/authentification',
+    component: authentification,
+  },
  ]
 })
