7979e32cb7a20639d2a23e3f431b302753915e74
ETombuyses <e.tombuyses@gmail.com>
2019-06-20 10:41:19 +0200

    merge emilie

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="app">
-    <!-- <the-header></the-header>
-    <document v-if="mathematics" :paramter="takeJsonId"></document>
-    <div class="big-circle">
-      <div class="big-circle__small-circle" @click="mathematics = true"></div>
-    </div> -->
-    
-    <router-link to="/">Home</router-link>
-    <router-link to="document/1">Document</router-link>
-    <router-link to="earth">Earth</router-link>
     <router-view></router-view>
     <footer> 
       <ul>
@@ -45,33 +36,12 @@
 </template>
 
 <script>
-// import TheHeader from './components/TheHeader.vue'
-// import Document from './components/Document.vue'
 
 export default {
-  // name: 'app',
-  // components: {
-  //   TheHeader,
-  //   Document
-  // },
-  // data(){
-  //   return {
-  //     mathematics : false,
-  //   }
-  // },
-  // methods : {
-
-  // }
 }
 </script>
 
 <style lang='scss' scoped>
-  // #app {
-  //   display: flex;
-  //   flex-direction: column;
-  //   justify-content: center;
-  //   align-items: center;
-  // }
   footer {
     position: absolute;
     bottom: 0;
